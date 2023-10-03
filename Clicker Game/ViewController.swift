d2f096bd8bdf202a2d6c33dee29267fe9b9332ce
Annahlu Raclawski <annahlu47@gmail.com>
2023-10-03 09:10:04 -0500

    Initial Commit

---
+++ Clicker Game/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Clicker Game
+//
+//  Created by ANNAHLU RACLAWSKI on 10/3/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
