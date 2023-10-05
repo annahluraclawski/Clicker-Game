f445029dc000825f3a056a3108d97ebe3c991e3a
Annahlu Raclawski <annahlu47@gmail.com>
2023-10-05 09:47:03 -0500

    In class October 5

--- Clicker Game/ViewController.swift
+++ Clicker Game/ViewController.swift
@@ -8,12 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var points = 0
+    
+    @IBOutlet weak var pointsOutlet: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
-
-
+    
+    
+    @IBAction func clickButton(_ sender: Any) {
+        pointsOutlet.text = "\(points)"
+        points+=1
+    }
+    
+    @IBAction func storeButton(_ sender: Any) {
+        performSegue(withIdentifier: "toStore", sender: self)
+    }
+    
+    
 }
 
